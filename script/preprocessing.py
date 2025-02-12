22e4f7a6ea5c648c6dd1390742785ac08d09d753
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2025-02-12 23:44:19 +0530

    Updated

--- script/preprocessing.py
+++ script/preprocessing.py
@@ -23,10 +23,16 @@ current_dir = os.getcwd()
 
 # Load the data
 data_dir = os.path.join(current_dir, "..", 'data')
-program_data = pd.read_csv(os.path.join(data_dir, 'program_qna.csv'))
+program_data = pd.read_csv(os.path.join(data_dir, 'program_qna.csv'), sep=',', quotechar='"',)
 greetings_data = pd.read_csv(os.path.join(data_dir, 'greetings.csv'))
 # -
 
 program_data.head()
+program_data.describe()
 
 greetings_data.head()
+
+# ### Check for Duplicates
+
+program_data = program_data.drop_duplicates()
+program_data.describe()
